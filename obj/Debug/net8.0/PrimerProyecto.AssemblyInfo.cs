68b14c837bae9a8998990ee91df18f05e96715b5
FRANCO CAPPELLA <49123639@ort>
2025-10-09 14:29:06 -0300

    asdsda

--- obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce500603336ee34cf3345b15492b0d9bb948372")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
