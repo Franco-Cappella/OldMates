bc73355457eb9fdcd879b436d5aa7eb066412964
FRANCO CAPPELLA <49123639@ort>
2025-10-23 14:27:20 -0300

    terminando actividades y layout

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PrimerProyecto.Models;
+
+namespace PrimerProyecto.Controllers;
+
+public class AccountController : Controller
+{
+}
