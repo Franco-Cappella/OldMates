1961c1c4bbc528887c1085a16b061a1e23c127e7
BRUNO EZEQUIEL ROTH <49004085@ort>
2025-11-20 13:33:42 -0300

    f

--- obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192ce0abcc6fd7effb0e3446b31188521e178a76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12f54064a9cd6cea3fba846056185c199e0556a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
